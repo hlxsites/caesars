18b60be74af9889afd53aa929b12bc7159040485
Tania Mathern <mathern@adobe.com>
2023-02-23 09:30:25 -0800

    Modify CSS

--- blocks/highlight-card/highlight-card.js
+++ blocks/highlight-card/highlight-card.js
@@ -2,6 +2,4 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   console.log('Decorating highlight-card block');
-
-  // text content has background color 141414
 }
