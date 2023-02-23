449924c5f7b246c2ab8c5fb25b0bf840c152223b
Tania Mathern <mathern@adobe.com>
2023-02-23 10:10:31 -0800

    Add classes

--- blocks/highlight-card/highlight-card.js
+++ blocks/highlight-card/highlight-card.js
@@ -2,4 +2,13 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   console.log('Decorating highlight-card block');
+
+  let childRowNumber = 0;
+  [...block.children].forEach((row) => {
+    if (childRowNumber === 0) row.classList.add(`highlight-card-image`);
+    else row.classList.add(`highlight-card-text`);
+    ++childRowNumber;
+  });
+
+  // img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
 }
