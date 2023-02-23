ddc0e4c8762d1e6f8fe007bf1c873c59135be670
Tania Mathern <mathern@adobe.com>
2023-02-23 11:20:04 -0800

    Refactor CSS

--- blocks/highlight-card/highlight-card.js
+++ blocks/highlight-card/highlight-card.js
@@ -3,14 +3,14 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 export default function decorate(block) {
   console.log('Decorating highlight-card block');
 
-  // block.querySelectorAll('img').forEach((img) => {
-  //   img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '960' }]))
-  // });
+  block.querySelectorAll('img').forEach((img) => {
+    img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '1600' }]))
+  });
 
   let childRowNumber = 0;
   [...block.children].forEach((row) => {
-    if (childRowNumber === 0) row.classList.add(`highlight-card-image`);
-    else row.classList.add(`highlight-card-text`);
+    if (childRowNumber === 0) row.classList.add('highlight-card-image');
+    else row.classList.add('highlight-card-text');
     ++childRowNumber;
   });
 }
