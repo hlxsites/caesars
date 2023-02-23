7cfccbb29c312544c2765746a3d6aa0775947346
Tania Mathern <mathern@adobe.com>
2023-02-23 09:17:55 -0800

    Renamde CTA to highlight-card

---
+++ blocks/highlight-card/highlight-card.js
@@ -0,0 +1,7 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
+export default function decorate(block) {
+  console.log('Decorating highlight-card block');
+
+  // text content has background color 141414
+}
