f4a0845ac94ab86c87ac55cd2820c21d8b89a76b
Chris Borland <borland@adobe.com>
2023-02-24 16:26:49 -0500

    linting

--- caesars-palace/blocks/intro-section/intro-section.js
+++ caesars-palace/blocks/intro-section/intro-section.js
@@ -4,17 +4,17 @@ export default function decorate(block) {
   const rows = [...block.children];
   // section inner
   const innerContainer = rows[0];
-  innerContainer.classList.add('intro-section--inner');
+  innerContainer.classList.add('intro-section-inner');
   // content
   const mainChildren = [...innerContainer.children];
-  mainChildren[0].classList.add('intro-section--content');
+  mainChildren[0].classList.add('intro-section-content');
   // main image
   const mainImageContainer = mainChildren[1];
-  mainImageContainer.classList.add('intro-section--image-container');
+  mainImageContainer.classList.add('intro-section-image-container');
   // secondary image.  move to section inner.
   const secondImageContainer = rows[1];
   const secondImageDiv = secondImageContainer.firstElementChild;
-  secondImageDiv.classList.add('intro-section--secondary-image-container');
+  secondImageDiv.classList.add('intro-section-secondary-image-container');
   innerContainer.append(secondImageDiv);
   secondImageContainer.remove();
   block.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
