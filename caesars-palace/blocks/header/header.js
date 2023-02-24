0f47e6e47aa836a98322a4bed34ab01fd1a5d732
Amol Anand <amol-anand@users.noreply.github.com>
2023-02-24 15:17:13 -0500

    Issue 23 restructure code (#26)

--- caesars-palace/blocks/header/header.js
+++ caesars-palace/blocks/header/header.js
@@ -93,7 +93,7 @@ export default async function decorate(block) {
   block.textContent = '';
 
   // fetch nav content
-  const navPath = getMetadata('nav') || '/nav';
+  const navPath = getMetadata('nav') || '/caesars-palace/nav';
   const resp = await fetch(`${navPath}.plain.html`, window.location.pathname.endsWith('/nav') ? { cache: 'reload' } : {});
 
   if (resp.ok) {
