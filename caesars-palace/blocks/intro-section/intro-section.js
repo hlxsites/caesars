12c1ffc372f26c3bf0d8734e065704940e186ec0
Chris Borland <borland@adobe.com>
2023-02-24 16:15:41 -0500

    linting

--- caesars-palace/blocks/intro-section/intro-section.js
+++ caesars-palace/blocks/intro-section/intro-section.js
@@ -1,27 +1,21 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
-    const rows = [...block.children];
-
-    // section inner
-    const innerContainer = rows[0];
-    innerContainer.classList.add('intro-section__inner');
-
-    // content
-    const mainChildren = [...innerContainer.children];
-    mainChildren[0].classList.add('intro-section__content');
-
-    // main image
-    const mainImageContainer = mainChildren[1];
-    mainImageContainer.classList.add('intro-section__image-container');
-
-    // secondary image.  move to section inner.
-    const secondImageContainer = rows[1];
-    const secondImageDiv = secondImageContainer.firstElementChild;
-    secondImageDiv.classList.add('intro-section__secondary-image-container');
-    innerContainer.append(secondImageDiv);
-    secondImageContainer.remove();
-
-    block.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-
+  const rows = [...block.children];
+  // section inner
+  const innerContainer = rows[0];
+  innerContainer.classList.add('intro-section__inner');
+  // content
+  const mainChildren = [...innerContainer.children];
+  mainChildren[0].classList.add('intro-section__content');
+  // main image
+  const mainImageContainer = mainChildren[1];
+  mainImageContainer.classList.add('intro-section__image-container');
+  // secondary image.  move to section inner.
+  const secondImageContainer = rows[1];
+  const secondImageDiv = secondImageContainer.firstElementChild;
+  secondImageDiv.classList.add('intro-section__secondary-image-container');
+  innerContainer.append(secondImageDiv);
+  secondImageContainer.remove();
+  block.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
 }
