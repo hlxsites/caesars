6d649a5fc68c2605a06316653a7e14d9f084b0c4
Chris Borland <borland@adobe.com>
2023-02-24 14:48:20 -0500

    - re-ordered css to be mobile first. - cleaned up js

--- blocks/intro-section/intro-section.js
+++ blocks/intro-section/intro-section.js
@@ -1,21 +1,6 @@
 export default function decorate(block) {
     const rows = [...block.children];
 
-    // outer container
-    /*
-    const outerContainer = document.createElement('div');
-    outerContainer.classList.add('container');
-    */
-
-    // background image
-    /*
-    const backgroundContainer = rows[0];
-    const backgroundImage = backgroundContainer.querySelector('img').src;
-    block.style.backgroundImage = 'url(' + backgroundImage + ')';
-    block.classList.add('full-width');
-    backgroundContainer.remove();
-    */
-
     // section inner
     const innerContainer = rows[0];
     innerContainer.classList.add('intro-section__inner');
@@ -31,15 +16,8 @@ export default function decorate(block) {
     // secondary image.  move to section inner.
     const secondImageContainer = rows[1];
     const secondImageDiv = secondImageContainer.firstElementChild;
-    secondImageDiv.classList.add('intro-section__image-container');
     secondImageDiv.classList.add('intro-section__secondary-image-container');
     innerContainer.append(secondImageDiv);
     secondImageContainer.remove();
-
-    /*
-    outerContainer.appendChild(innerContainer);
-
-    block.appendChild(outerContainer);
-    */
 
 }
