523c327ce9ee41b3a327a03734d36282b5e1616d
Tania Mathern <mathern@adobe.com>
2023-02-24 09:14:41 -0800

    Change order to grab elements

--- blocks/highlight-card/highlight-card.js
+++ blocks/highlight-card/highlight-card.js
@@ -5,14 +5,14 @@ export default function decorate(block) {
     img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '1600' }]));
   });
 
+  block.querySelectorAll('a').forEach((buttonLink) => {
+    buttonLink.closest('div').classList.add('highlight-card-action');
+  });
+
   let childRowNumber = 0;
   [...block.children].forEach((row) => {
     if (childRowNumber !== 0) row.classList.add('highlight-card-text');
     else row.classList.add('highlight-card-image');
     childRowNumber += 1;
-  });
-
-  block.querySelectorAll('a').forEach((buttonLink) => {
-    buttonLink.closest('div').classList.add('highlight-card-action');
   });
 }
