f7bd5e65328420887de69c9f34485457e710be29
Chris Borland <borland@adobe.com>
2023-02-24 14:25:41 -0500

    - Update line-height to 1.4 (from 1.1) - Removed background from block - Added media queries (not working)

--- blocks/intro-section/intro-section.js
+++ blocks/intro-section/intro-section.js
@@ -1,31 +1,45 @@
 export default function decorate(block) {
     const rows = [...block.children];
-    
+
+    // outer container
+    /*
+    const outerContainer = document.createElement('div');
+    outerContainer.classList.add('container');
+    */
+
     // background image
-    rows[0].classList.add('intro-section-background');
-    rows[0].firstElementChild.className = 'intro-section-background-image-container';
-    const backgroundImage = rows[0].querySelector('img').src;
+    /*
+    const backgroundContainer = rows[0];
+    const backgroundImage = backgroundContainer.querySelector('img').src;
     block.style.backgroundImage = 'url(' + backgroundImage + ')';
     block.classList.add('full-width');
-    rows[0].remove();
-
-
+    backgroundContainer.remove();
+    */
 
     // section inner
-    const innerSection = rows[1];
-    innerSection.classList.add('intro-section-inner');
-    innerSection.classList.add('container');
+    const innerContainer = rows[0];
+    innerContainer.classList.add('intro-section__inner');
 
-    const mainChildren = [...rows[1].children];
-    mainChildren[0].classList.add('intro-section-main-content');
-    const mainImage = mainChildren[1];
-    mainImage.classList.add('intro-section-main-image-container');
-    mainImage.firstElementChild.classList.add('intro-section-main-image');
+    // content
+    const mainChildren = [...innerContainer.children];
+    mainChildren[0].classList.add('intro-section__content');
 
-    const secondImageDiv = rows[2].firstElementChild;
-    secondImageDiv.classList.add('intro-section-secondary-image-container');
-    secondImageDiv.firstElementChild.classList.add('intro-section-secondary-image');
-    innerSection.append(secondImageDiv);
-    rows[2].remove();
+    // main image
+    const mainImageContainer = mainChildren[1];
+    mainImageContainer.classList.add('intro-section__image-container');
+
+    // secondary image.  move to section inner.
+    const secondImageContainer = rows[1];
+    const secondImageDiv = secondImageContainer.firstElementChild;
+    secondImageDiv.classList.add('intro-section__image-container');
+    secondImageDiv.classList.add('intro-section__secondary-image-container');
+    innerContainer.append(secondImageDiv);
+    secondImageContainer.remove();
+
+    /*
+    outerContainer.appendChild(innerContainer);
+
+    block.appendChild(outerContainer);
+    */
 
 }
