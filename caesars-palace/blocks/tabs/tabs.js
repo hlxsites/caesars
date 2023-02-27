0c5749b26ce31ebdfa9d6c39bbda1e9fd184801d
Tania Mathern <mathern@adobe.com>
2023-02-27 15:46:02 -0800

    add classes to tabs

--- caesars-palace/blocks/tabs/tabs.js
+++ caesars-palace/blocks/tabs/tabs.js
@@ -1,3 +1,23 @@
 export default function decorate(block) {
-  console.log("Decorating tabs block");
+  // http://localhost:3000/caesars-palace/drafts/tmathern/tabs
+
+  block.querySelectorAll('img').forEach((tabImage) => {
+    tabImage.closest('div').classList.add('tab-image');
+  });
+
+  [...block.children].forEach((row, i) => {
+    [...row.children].forEach((contentElement, i) => {
+      if(![...contentElement.classList].includes('tab-image')){
+        if(contentElement.innerHTML){
+          if(i === 0){
+            contentElement.closest('div').classList.add('tab-title');
+          } else {
+            contentElement.closest('div').classList.add('tab-text');
+          }
+        } else {
+          contentElement.remove();
+        }
+      }
+    });
+  });
 }
