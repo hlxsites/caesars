0ec82f8edf01e132a97032f96b75ea83f00abdfe
Tania Mathern <mathern@adobe.com>
2023-02-27 10:18:08 -0800

    WIP

--- blocks/highlight-card/highlight-card.js
+++ blocks/highlight-card/highlight-card.js
@@ -14,45 +14,42 @@ export default function decorate(block) {
     childRowNumber += 1;
   });
 
-  
-
-  const mediaWidthQueryMatcher  = window.matchMedia('only screen and (max-width: 960px)');
-  const mediaWidthChangeHandler = (event) => {
-    const wrapElement = (element, wrapper) => {
-      if (element && element.parentNode) {
-        element.parentNode.insertBefore(wrapper, element);
-        wrapper.appendChild(element);
-      }
-    };
-
-    const unwrapElement = (element, wrapper) => {
-    };
-
-    if(event.matches){
-      let elementsToWrap = document.getElementsByClassName('highlight-card-text');
-      [...elementsToWrap].forEach(wrappedElement => {
-        const anchorElement = document.createElement("a");
-        anchorElement.href = targetLink;
-        const wrapper = anchorElement;
-
-        wrapElement(wrappedElement, wrapper);
-      });
-
-      elementsToWrap = document.getElementsByClassName('highlight-card-image');
-      [...elementsToWrap].forEach(wrappedElement => {
-        const anchorElement = document.createElement("a");
-        anchorElement.href = targetLink;
-        const wrapper = anchorElement;
-
-        wrapElement(wrappedElement, wrapper);
-      });
-    } else {
-      
+  const wrapElement = (element, wrapper) => {
+    if (element && element.parentNode) {
+      element.parentNode.insertBefore(wrapper, element);
+      wrapper.appendChild(element);
     }
-  }
-  mediaWidthChangeHandler(mediaWidthQueryMatcher);
+  };
 
-  mediaWidthQueryMatcher.addEventListener('change', (event) => {
-    mediaWidthChangeHandler(event);
-  });
+  const unwrapElement = (element, parent, wrapper) => {
+  };
+
+  console.log('----')
+  console.log(block.childNodes);
+  console.log('----')
+
+  // const mediaWidthQueryMatcher  = window.matchMedia('only screen and (max-width: 960px)');
+  
+  // const mediaWidthChangeHandler = (event) => {
+  //   const wrapElement = (element, wrapper) => {
+  //     if (element && element.parentNode) {
+  //       element.parentNode.insertBefore(wrapper, element);
+  //       wrapper.appendChild(element);
+  //     }
+  //   };
+
+  //   const unwrapElement = (element, parent, wrapper) => {
+
+  //   };
+
+  //   if(event.matches){
+  //   } else {
+      
+  //   }
+  // }
+  // mediaWidthChangeHandler(mediaWidthQueryMatcher);
+
+  // mediaWidthQueryMatcher.addEventListener('change', (event) => {
+  //   mediaWidthChangeHandler(event);
+  // });
 }
