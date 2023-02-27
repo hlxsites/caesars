38b257deaa4a0248019ec1edb3cabd1acb8ebd6b
Tania Mathern <mathern@adobe.com>
2023-02-27 10:57:36 -0800

    Fix links

--- caesars-palace/blocks/highlight-card/highlight-card.js
+++ caesars-palace/blocks/highlight-card/highlight-card.js
@@ -7,51 +7,34 @@ export default function decorate(block) {
     buttonLink.closest('div').classList.add('button-container');
   });
 
-  const link = document.createElement('a');
-  link.classList.add('highlight-card-link');
-  link.href = targetLink;
-
   [...block.children].forEach((row, i) => {
     if (i !== 0) row.classList.add('highlight-card-text');
     else row.classList.add('highlight-card-image');
   });
-  link.innerHTML = block.innerHTML;
-  block.innerHTML = '';
-  block.append(link);
-  // const wrapElement = (element, wrapper) => {
-  //   if (element && element.parentNode) {
-  //     element.parentNode.insertBefore(wrapper, element);
-  //     wrapper.appendChild(element);
-  //   }
-  // };
 
-  // const unwrapElement = (element, parent, wrapper) => {
-  // };
+  
 
-  console.log('----');
-  console.log(block.innerHTML);
-  console.log('----');
+  const mediaWidthQueryMatcher  = window.matchMedia('only screen and (max-width: 960px)');
+  const mediaWidthChangeHandler = (event) => {
+    if(event.matches){
+      const link = document.createElement('a');
+      link.classList.add('highlight-card-link');
+      link.href = targetLink;
 
-  // const mediaWidthQueryMatcher  = window.matchMedia('only screen and (max-width: 960px)');
-  // const mediaWidthChangeHandler = (event) => {
-  //   const wrapElement = (element, wrapper) => {
-  //     if (element && element.parentNode) {
-  //       element.parentNode.insertBefore(wrapper, element);
-  //       wrapper.appendChild(element);
-  //     }
-  //   };
+      link.innerHTML = block.innerHTML;
+      block.innerHTML = '';
+      block.append(link);
+    } else {
+      const wrapper = block.firstChild;
+      if(block.firstChild && block.firstChild.href){
+        const wrappedContent = wrapper.innerHTML;
+        block.innerHTML = wrappedContent;
+      }
+    }
+  }
 
-  //   const unwrapElement = (element, parent, wrapper) => {
-
-  //   };
-
-  //   if(event.matches){
-  //   } else {
-  //   }
-  // }
-  // mediaWidthChangeHandler(mediaWidthQueryMatcher);
-
-  // mediaWidthQueryMatcher.addEventListener('change', (event) => {
-  //   mediaWidthChangeHandler(event);
-  // });
+  mediaWidthChangeHandler(mediaWidthQueryMatcher);
+  mediaWidthQueryMatcher.addEventListener('change', (event) => {
+    mediaWidthChangeHandler(event);
+  });
 }
