8836c8bace9494c918d9074c1c5812f0981f58fe
Tania Mathern <mathern@adobe.com>
2023-02-27 15:59:54 -0800

    Linting

--- caesars-palace/blocks/tabs/tabs.js
+++ caesars-palace/blocks/tabs/tabs.js
@@ -1,23 +1,23 @@
 export default function decorate(block) {
   // http://localhost:3000/caesars-palace/drafts/tmathern/tabs
 
-  [...block.children].forEach((row, i) => {
+  [...block.children].forEach((row) => {
     [...row.children].forEach((contentElement, i) => {
-      if(contentElement.innerHTML){
-          if(i === 0){
-            contentElement.closest('div').classList.add('tab-title');
-          } else {
-            if(contentElement.children 
-              && contentElement.children.length>0 
-              && contentElement.children[0].tagName === 'PICTURE'){
-              contentElement.closest('div').classList.add('tab-image');
-            } else {
-              contentElement.closest('div').classList.add('tab-text');
-            }
-          }
+      if (contentElement.innerHTML) {
+        if (i === 0) {
+          contentElement.closest('div').classList.add('tab-title');
         } else {
-          contentElement.remove();
+          if (contentElement.children
+            && contentElement.children.length > 0
+            && contentElement.children[0].tagName === 'PICTURE') {
+            contentElement.closest('div').classList.add('tab-image');
+          } else {
+            contentElement.closest('div').classList.add('tab-text');
+          }
         }
+      } else {
+        contentElement.remove();
+      }
     });
   });
 }
