fb39c9dff491f35f0707c4c4a0fa4f3ac0b4b364
Tania Mathern <mathern@adobe.com>
2023-02-27 15:52:35 -0800

    Change dom handling

--- caesars-palace/blocks/tabs/tabs.js
+++ caesars-palace/blocks/tabs/tabs.js
@@ -1,23 +1,15 @@
 export default function decorate(block) {
   // http://localhost:3000/caesars-palace/drafts/tmathern/tabs
 
-  block.querySelectorAll('img').forEach((tabImage) => {
-    tabImage.closest('div').classList.add('tab-image');
-  });
-
   [...block.children].forEach((row, i) => {
     [...row.children].forEach((contentElement, i) => {
-      if(![...contentElement.classList].includes('tab-image')){
-        if(contentElement.innerHTML){
+      if(contentElement.innerHTML){
           if(i === 0){
             contentElement.closest('div').classList.add('tab-title');
-          } else {
-            contentElement.closest('div').classList.add('tab-text');
           }
         } else {
           contentElement.remove();
         }
-      }
     });
   });
 }
