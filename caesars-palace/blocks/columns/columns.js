4df6877e0829c71be16ec837b3a0bb736055acd5
Anthony Rumsey <arumsey@adobe.com>
2023-02-28 15:00:37 -0500

    Columns (Variations) #13 (#28)
    - `cet-card` variant
    - `stretch-start` variant
    - `stretch-end` variant

--- caesars-palace/blocks/columns/columns.js
+++ caesars-palace/blocks/columns/columns.js
@@ -1,4 +1,15 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+
+  if (block.classList.contains('cet-card') && cols.length === 2) {
+    // image placement
+    if (cols[0].querySelector('picture')) {
+      block.classList.add('image-start');
+    } else if (cols[1].querySelector('picture')) {
+      block.classList.add('image-end');
+    }
+    // button group
+    block.querySelectorAll('a').forEach((anchor) => anchor.closest('p').classList.add('button-group'));
+  }
 }
