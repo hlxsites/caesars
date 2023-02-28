0f65f57d01f33a6ff6046ea713c94d83f866c3bf
Tania Mathern <mathern@adobe.com>
2023-02-28 08:44:00 -0800

    Move tab bar elements on top

--- caesars-palace/blocks/tabs/tabs.js
+++ caesars-palace/blocks/tabs/tabs.js
@@ -1,16 +1,24 @@
-function openTab(e) {
+function showTab(e) {
   console.log("Show tab");
 }
 
 export default function decorate(block) {
   // http://localhost:3000/caesars-palace/drafts/tmathern/tabs
+  const tabTitles = document.createElement('div');
+  tabTitles.classList.add('tab-navbar');
 
-  [...block.children].forEach((row) => {
+  [...block.children].forEach((row, rowIndex) => {
     row.classList.add('tab');
+    row.classList.add(`tab-content-${rowIndex}`);
+
     [...row.children].forEach((contentElement, i) => {
       if (contentElement.innerHTML) {
         if (i === 0) {
-          contentElement.closest('div').classList.add('tab-title');
+          const divToMove = contentElement.closest('div');
+          divToMove.classList.add('tab-title');
+          divToMove.classList.add(`tab-navbar-element-${rowIndex}`);
+
+          tabTitles.appendChild(divToMove);
         } else if (contentElement.children
           && contentElement.children.length > 0
           && contentElement.children[0].tagName === 'PICTURE') {
@@ -23,4 +31,6 @@ export default function decorate(block) {
       }
     });
   });
+
+  block.prepend(tabTitles);
 }
