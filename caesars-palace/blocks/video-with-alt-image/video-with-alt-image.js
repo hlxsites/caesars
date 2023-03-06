a4ab574ce2a97116c8ef7d9d8781698aa9a3e2bc
Tania Mathern <mathern@adobe.com>
2023-03-06 11:25:45 -0800

    Media query listener

--- caesars-palace/blocks/video-with-alt-image/video-with-alt-image.js
+++ caesars-palace/blocks/video-with-alt-image/video-with-alt-image.js
@@ -2,11 +2,29 @@
 
 export default function decorate(block) {
   // turn link into video tag 
+  /* 
+  <video autoplay="" loop="" playsinline=""><source src="/content/dam/empire/clv/things-to-do/nightlife/omnia-nightclub/video/clv-omnia-main-room.mp4" type="video/mp4"></video>
+  */ 
+
   block.querySelectorAll('a').forEach((videoLink) => {
-    console.log(videoLink);
+    videoLink.closest('div').classList.add('video-link');
   });
 
-  // show image on mobile by default, video is hidden
+  block.querySelectorAll('img').forEach((image) => {
+    image.closest('div').classList.add('video-alt-image');
+  });
 
-  // show video only if on large screen, image is hidden, change classes on video tag
+  const mediaWidthQueryMatcher = window.matchMedia('only screen and (min-width: 1170px)');
+  const mediaWidthChangeHandler = (event) => {
+    if (event.matches === false) {
+      console.log("Event matches");
+    } else {
+      console.log("Event does not match");
+    }
+  };
+
+  mediaWidthChangeHandler(mediaWidthQueryMatcher);
+  mediaWidthQueryMatcher.addEventListener('change', (event) => {
+    mediaWidthChangeHandler(event);
+  });
 }
