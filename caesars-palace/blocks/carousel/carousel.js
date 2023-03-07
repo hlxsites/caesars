906405082e03e7cb39eb07e152d58eea10269c10
Tania Mathern <mathern@adobe.com>
2023-03-07 14:04:04 -0800

    Build DOM for carousel

--- caesars-palace/blocks/carousel/carousel.js
+++ caesars-palace/blocks/carousel/carousel.js
@@ -1,15 +1,13 @@
-/* Breakpoint for desktop at 1170px */
-
 export default function decorate(block) {
-  [...block.children].forEach((row) => {
+  [...block.children].forEach((row, rowIndex) => {
     row.classList.add('carousel-element');
+    row.classList.add(`carousel-element-${rowIndex}`);
 
     const imagesInRow = row.querySelectorAll('img');
     if(imagesInRow.length === 1){
       const carouselImage = imagesInRow[0];
-      image.closest('div').classList.add('carousel-image');
-      image.closest('div').classList.add('carousel-main-image');
-      image.closest('div').classList.add('carousel-alt-image');
+      carouselImage.closest('div').classList.add('carousel-image');
+      carouselImage.closest('div').classList.add('carousel-only-image');
     } else if(imagesInRow.length === 2){
       const carouselImage = imagesInRow[0];
       carouselImage.closest('div').classList.add('carousel-image');
@@ -19,5 +17,15 @@ export default function decorate(block) {
       carouselAltImage.closest('div').classList.add('carousel-image');
       carouselAltImage.closest('div').classList.add('carousel-alt-image');
     }
+
+    [...row.children].forEach((item) => {
+      if (item.innerHTML) {
+        if(![...item.classList].includes('carousel-image')){
+          item.classList.add('carousel-text');
+        }
+      } else {
+        item.remove();
+      }
+    });
   });
 }
