b8080efb607e37d670fae5614c4d0b3a04c4186a
Tania Mathern <mathern@adobe.com>
2023-03-07 13:44:23 -0800

    Handle alt images in carousel

--- caesars-palace/blocks/carousel/carousel.js
+++ caesars-palace/blocks/carousel/carousel.js
@@ -1,1 +1,23 @@
 /* Breakpoint for desktop at 1170px */
+
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    row.classList.add('carousel-element');
+
+    const imagesInRow = row.querySelectorAll('img');
+    if(imagesInRow.length === 1){
+      const carouselImage = imagesInRow[0];
+      image.closest('div').classList.add('carousel-image');
+      image.closest('div').classList.add('carousel-main-image');
+      image.closest('div').classList.add('carousel-alt-image');
+    } else if(imagesInRow.length === 2){
+      const carouselImage = imagesInRow[0];
+      carouselImage.closest('div').classList.add('carousel-image');
+      carouselImage.closest('div').classList.add('carousel-main-image');
+
+      const carouselAltImage = imagesInRow[1];
+      carouselAltImage.closest('div').classList.add('carousel-image');
+      carouselAltImage.closest('div').classList.add('carousel-alt-image');
+    }
+  });
+}
