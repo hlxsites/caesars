560c1d07d9feca985c52c1ec63d81b0a4ad79c10
jfait <jfait@adobe.com>
2023-03-10 11:21:12 -0500

    Issue45 FAQ Block : Accordian like faq block -linting fix

--- caesars-palace/blocks/faq/faq.js
+++ caesars-palace/blocks/faq/faq.js
@@ -8,7 +8,7 @@ export default function decorate(block) {
     row.setAttribute('tabindex', i);
     row.setAttribute('aria-expanded', false);
     row.classList.add('questions-content');
-    row.addEventListener('click', function () {
+    row.addEventListener('click', () => {
       const button = row.querySelector('[role="button"]');
       const span = row.querySelector('span');
       if (row.getAttribute('aria-expanded') === 'true') {
@@ -22,14 +22,13 @@ export default function decorate(block) {
         button.setAttribute('aria-pressed', true);
         span.classList.add('expanded');
       }
-
     });
 
     const questionContentText = document.createElement('div');
     questionContentText.classList.add('questions-content-text');
 
     Array.from(row.children)
-      .forEach(childElement => {
+      .forEach((childElement) => {
         questionContentText.appendChild(childElement);
       });
 
