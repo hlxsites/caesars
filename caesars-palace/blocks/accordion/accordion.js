1e173eaca875c1089d54be496bc621b3b84f00c1
Tania Mathern <mathern@adobe.com>
2023-03-13 14:42:34 -0700

    Fet star item

---
+++ caesars-palace/blocks/accordion/accordion.js
@@ -0,0 +1,17 @@
+let starItem = 1;
+
+export default function decorate(block) {
+  // first line is configuration of star item to be open/selected by default
+
+  const configuredStarElement = parseInt(block.children[0].innerText, 10);
+  if (!Number.isNaN(configuredStarElement)
+    && Number.isInteger(configuredStarElement)) {
+      starItem = configuredStarElement;
+  }
+  block.children[0].remove();
+  console.log("Star item is ", starItem);
+
+  [...block.children].forEach((row) => {
+    console.log(row);
+  });
+}
