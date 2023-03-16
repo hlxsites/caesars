e97d154d1d03ea881476fc8fb8dbbceafed2eb81
Tania Mathern <mathern@adobe.com>
2023-03-16 10:12:01 -0700

    Prepare css

--- caesars-palace/blocks/featured-card/featured-card.js
+++ caesars-palace/blocks/featured-card/featured-card.js
@@ -4,15 +4,15 @@ export default function decorate(block) {
   backgroundImageElement.classList.add('featured-card-background');
 
   const blockContent = blockElements[1];
-  blockContent.classList.add('featured-card-feature');
+  blockContent.classList.add('featured-card-content');
 
   blockContent.querySelectorAll('img').forEach((image) => {
     image.closest('div').classList.add('featured-card-image');
   });
 
   [...blockContent.children].forEach((item) => {
-    if(![...item.classList].includes('featured-card-image')){
-      item.classList.add('featured-card-content');
+    if (![...item.classList].includes('featured-card-image')) {
+      item.classList.add('featured-card-text');
     }
   });
 }
