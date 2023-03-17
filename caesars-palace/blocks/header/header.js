7086c6dc13da1cd18a978f24480ed3f9b671253a
Amol Anand <amol.anand@gmail.com>
2023-03-16 23:23:20 -0400

    moving resources to scripts

--- caesars-palace/blocks/header/header.js
+++ caesars-palace/blocks/header/header.js
@@ -5,7 +5,7 @@ const isDesktop = window.matchMedia('(min-width: 1170px)');
 // const MAX_NAV_ITEMS_DESKTOP = 6;
 const CAESARS_DOT_COM = 'https://www.caesars.com';
 const GLOBAL_HEADER_JSON = '/content/empire/en/jcr:content/root/header.model.json';
-const GLOBAL_HEADER_JSON_LOCAL = '../../resources/header.model.json';
+const GLOBAL_HEADER_JSON_LOCAL = '../../scripts/resources/header.model.json';
 const GLOBAL_HEADER_LOGO_LOCAL = '../../icons/caesars-global-logo.svg';
 
 async function createGlobalNavLogo(logoFileReference) {
