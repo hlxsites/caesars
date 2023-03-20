14088be85be40ae337de1de29909a09e5b368688
Tania Mathern <mathern@adobe.com>
2023-03-20 13:14:26 -0700

    ingle line stats behaving ok

--- caesars-palace/blocks/stats/stats.js
+++ caesars-palace/blocks/stats/stats.js
@@ -1,11 +1,32 @@
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     const columnsInRow = [...row.children].length;
+    if(columnsInRow < 2){
+      row.style = `grid-template-columns: repeat(${columnsInRow}, 1fr)`;
+    }
+
+    const mediaSmallWidthQueryMatcher = window.matchMedia('only screen and (max-width: 767px)');
+    const mediaSmallWidthChangeHandler = async (event) => {
+      if (event.matches === false) {
+        console.log("no match: 768px");
+        row.style = '';
+      } else {
+        console.log("MATCH: 768px");
+        if(columnsInRow < 2){
+          row.style = `grid-template-columns: repeat(${columnsInRow}, 1fr)`;
+        }
+      }
+    };
+    mediaSmallWidthChangeHandler(mediaSmallWidthQueryMatcher);
+    mediaSmallWidthQueryMatcher.addEventListener('change', (event) => {
+      mediaSmallWidthChangeHandler(event);
+    });
 
     const mediaWidthQueryMatcher = window.matchMedia('only screen and (min-width: 768px)');
     const mediaWidthChangeHandler = async (event) => {
       if (event.matches === false) {
         console.log("no match: 768px");
+        row.style = '';
       } else {
         console.log("MATCH: 768px");
         if(columnsInRow < 3){
@@ -22,6 +43,7 @@ export default function decorate(block) {
     const mediaLargeWidthChangeHandler = async (event) => {
       if (event.matches === false) {
         console.log("no match: 960px");
+        row.style = '';
       } else {
         console.log("MATCH: 960px");
         if(columnsInRow < 5){
