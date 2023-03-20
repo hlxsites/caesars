3bd19373191b0581fe7e902c07124cd3df4a2c81
Tania Mathern <mathern@adobe.com>
2023-03-20 13:28:36 -0700

    Single line stats

--- caesars-palace/blocks/stats/stats.js
+++ caesars-palace/blocks/stats/stats.js
@@ -1,59 +1,48 @@
 export default function decorate(block) {
-  [...block.children].forEach((row) => {
-    const columnsInRow = [...row.children].length;
-    if(columnsInRow < 2){
-      row.style = `grid-template-columns: repeat(${columnsInRow}, 1fr)`;
-    }
+  const statsRows = [...block.children];
 
-    const mediaSmallWidthQueryMatcher = window.matchMedia('only screen and (max-width: 767px)');
-    const mediaSmallWidthChangeHandler = async (event) => {
-      if (event.matches === false) {
-        console.log("no match: 768px");
-        row.style = '';
-      } else {
-        console.log("MATCH: 768px");
-        if(columnsInRow < 2){
+  if (statsRows.length === 1) {
+    [...block.children].forEach((row) => {
+      const columnsInRow = [...row.children].length;
+
+      const mediaSmallWidthQueryMatcher = window.matchMedia('only screen and (max-width: 767px)');
+      const mediaSmallWidthChangeHandler = async (event) => {
+        if (event.matches === false) {
+          row.style = '';
+        } else if (columnsInRow < 2) {
           row.style = `grid-template-columns: repeat(${columnsInRow}, 1fr)`;
         }
-      }
-    };
-    mediaSmallWidthChangeHandler(mediaSmallWidthQueryMatcher);
-    mediaSmallWidthQueryMatcher.addEventListener('change', (event) => {
-      mediaSmallWidthChangeHandler(event);
-    });
+      };
+      mediaSmallWidthChangeHandler(mediaSmallWidthQueryMatcher);
+      mediaSmallWidthQueryMatcher.addEventListener('change', (event) => {
+        mediaSmallWidthChangeHandler(event);
+      });
 
-    const mediaWidthQueryMatcher = window.matchMedia('only screen and (min-width: 768px)');
-    const mediaWidthChangeHandler = async (event) => {
-      if (event.matches === false) {
-        console.log("no match: 768px");
-        row.style = '';
-      } else {
-        console.log("MATCH: 768px");
-        if(columnsInRow < 3){
+      const mediaWidthQueryMatcher = window.matchMedia('only screen and (min-width: 768px)');
+      const mediaWidthChangeHandler = async (event) => {
+        if (event.matches === false) {
+          row.style = '';
+        } else if (columnsInRow < 3) {
           row.style = `grid-template-columns: repeat(${columnsInRow}, 1fr)`;
         }
-      }
-    };
-    mediaWidthChangeHandler(mediaWidthQueryMatcher);
-    mediaWidthQueryMatcher.addEventListener('change', (event) => {
-      mediaWidthChangeHandler(event);
-    });
+      };
+      mediaWidthChangeHandler(mediaWidthQueryMatcher);
+      mediaWidthQueryMatcher.addEventListener('change', (event) => {
+        mediaWidthChangeHandler(event);
+      });
 
-    const mediaLargeWidthQueryMatcher = window.matchMedia('only screen and (min-width: 960px)');
-    const mediaLargeWidthChangeHandler = async (event) => {
-      if (event.matches === false) {
-        console.log("no match: 960px");
-        row.style = '';
-      } else {
-        console.log("MATCH: 960px");
-        if(columnsInRow < 5){
+      const mediaLargeWidthQueryMatcher = window.matchMedia('only screen and (min-width: 960px)');
+      const mediaLargeWidthChangeHandler = async (event) => {
+        if (event.matches === false) {
+          row.style = '';
+        } else if (columnsInRow < 5) {
           row.style = `grid-template-columns: repeat(${columnsInRow}, 1fr)`;
         }
-      }
-    };
-    mediaLargeWidthChangeHandler(mediaLargeWidthQueryMatcher);
-    mediaLargeWidthQueryMatcher.addEventListener('change', (event) => {
-      mediaLargeWidthChangeHandler(event);
+      };
+      mediaLargeWidthChangeHandler(mediaLargeWidthQueryMatcher);
+      mediaLargeWidthQueryMatcher.addEventListener('change', (event) => {
+        mediaLargeWidthChangeHandler(event);
+      });
     });
-  });
+  }
 }
