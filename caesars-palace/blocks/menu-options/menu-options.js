b32f35c549606d1a9fbff4ca66844790eeb60b8f
swetabar <swetabarman@gmail.com>
2023-04-06 14:31:17 -0400

    add additional changes + svg's

--- caesars-palace/blocks/menu-options/menu-options.js
+++ caesars-palace/blocks/menu-options/menu-options.js
@@ -4,4 +4,16 @@ export default function decorate(block) {
     const iconPdfParent = iconPdf.closest('p');
     iconPdfParent.classList.add('menu-link');
   });
+
+  const menuDivs = block.querySelectorAll('.menu-options > div > div');
+  [...menuDivs]
+    .forEach((menuDiv) => {
+      const h4Divs = menuDiv.querySelectorAll('h4');
+      const checkmarkDiv = document.createElement('div');
+      checkmarkDiv.classList.add('menu-rewards');
+      h4Divs.forEach((h4Div) => {
+        menuDiv.replaceChild(checkmarkDiv, h4Div);
+        checkmarkDiv.appendChild(h4Div);
+      });
+    });
 }
