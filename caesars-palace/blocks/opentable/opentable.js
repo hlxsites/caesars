981c1ef5b2b019e9cfdd026c62e322e9a7bafe4f
Amol Anand <amol.anand@gmail.com>
2023-04-09 14:00:54 -0400

    delaying opentable script

--- caesars-palace/blocks/opentable/opentable.js
+++ caesars-palace/blocks/opentable/opentable.js
@@ -1,15 +1,18 @@
+function embedOpentable(otWidgetLink, block) {
+  const otDiv = document.createElement('div');
+  otDiv.classList.add('otDiv');
+  const script = document.createElement('script');
+  script.src = otWidgetLink;
+  otDiv.append(script);
+  block.append(otDiv);
+}
+
 export default async function decorate(block) {
   const otWidgetLink = block.querySelector('a');
   if (otWidgetLink) {
     otWidgetLink.parentElement.remove();
-    const otDiv = document.createElement('div');
-    otDiv.classList.add('otDiv');
-    const script = document.createElement('script');
-    script.src = otWidgetLink;
-    otDiv.append(script);
-    block.append(otDiv);
+    window.setTimeout(() => embedOpentable(otWidgetLink, block), 3000);
   } else {
     block.remove();
   }
-  // window.setTimeout(() => embedMarketoForm(formId, divId), 3000);
 }
