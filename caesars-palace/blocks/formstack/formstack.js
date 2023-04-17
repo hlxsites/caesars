bf984f56efb2a241aefed311549e5e20b06a2bdf
Amol Anand <amol.anand@gmail.com>
2023-04-17 17:57:47 -0400

    working formstack!

--- caesars-palace/blocks/formstack/formstack.js
+++ caesars-palace/blocks/formstack/formstack.js
@@ -1,52 +1,92 @@
-const loadScript = (url, callback, attrs) => {
-    console.log("enters here")
-  const head = document.querySelector('head');
+let formstackTmp = '';
+
+function loadScript(url) {
+  const formstack = document.createElement('formstack');
   const script = document.createElement('script');
   script.src = url;
-  if (attrs) {
-    // eslint-disable-next-line no-restricted-syntax, guard-for-in
-    for (const attr in attrs) {
-      script.setAttribute(attr, attrs[attr]);
-    }
-  }
-  script.onload = callback;
-  console.log("script = " + script);
-  head.append(script);
-  return script;
-};
-
-const embedFormstackForm = () => {
-  // PDF Viewer for doc pages
-  loadScript(
-    'https://caesars.formstack.com/forms/js.php/linq_leisure_concierge_copy_copy',
-  );
-};
-
-export default async function decorate(block) {
-  const form = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        loadScript(
-          'https://caesars.formstack.com/forms/js.php/linq_leisure_concierge_copy_copy',
-        );
-      }
-    });
-  });
-  form.observe(block);
+  const noscript = document.createElement('noscript');
+  const noscriptAnchor = document.createElement('a');
+  noscriptAnchor.href = url;
+  noscriptAnchor.title = 'Online form';
+  formstack.append(script);
+  formstack.append(noscript);
+  window.formStackContent = {};
+  return formstack;
 }
 
-// export default function decorate(block) {
-//   const inquiryFormScript = document.createElement('script');
-//   inquiryFormScript.type = 'text/javascript';
-//   inquiryFormScript.src =
-//     'https://caesars.formstack.com/forms/js.php/linq_leisure_concierge_copy_copy';
-//   const inquiryFormNoScript = document.createElement('noscript');
-//   const aHref = document.createElement('a');
-//   aHref.title = 'Online Form';
-//   aHref.href =
-//     'https://caesars.formstack.com/forms/linq_leisure_concierge_copy_copy';
-//   inquiryFormNoScript.appendChild(aHref);
-//   aHref.textContent = 'Online Form - Caesars Palace Concierge';
-//   block.appendChild(inquiryFormScript);
-//   block.appendChild(inquiryFormNoScript);
-// }
+// Unfortunately, we have to override document.write calls from the formstack code
+// because we cannot move away from it and there are no other better options yet.
+// eslint-disable-next-line func-names
+document.write = function (s) {
+  formstackTmp = formstackTmp.concat(s);
+  window.formStackContent = {
+    value: formstackTmp,
+  };
+};
+
+// const formstack = document.querySelector('formstack');
+// formstack.innerHTML = formStackContent;
+
+// document.write = function (s) {
+//   const fsBodyEmbed = /.*?fsBody fsEmbed.*?/;
+//   const openDiv = /<div.*?>/;
+//   const closeDiv = /.*?<\/div>/;
+//   const formstack = document.querySelector('formstack');
+//   if (fsBodyEmbed.test(s)) {
+//     formstack.innerHTML += s;
+//   } else {
+//     const fsBody = formstack.querySelector(':scope > .fsBody.fsEmbed');
+//     if (openDiv.test(s)) {
+
+//     } else if (closeDiv.test(s)) {
+//       fsBody.innerHTML += s;
+//     }
+//     // console.log(`s is ${s}`);
+//   }
+// };
+
+// eslint-disable-next-line func-names
+// document.write = function (parameter) {
+//   if (!parameter) return;
+//   const scriptPattern = /<script.*?src=['|"](.*?)['|"]/;
+//   const stylePattern = /<style.*?type=['|"](.*?)['|"]/;
+//   if (scriptPattern.test(parameter)) {
+//     const srcAttribute = scriptPattern.exec(parameter)[1];
+//     const script = document.createElement('script');
+//     script.src = srcAttribute;
+//     document.head.appendChild(script);
+//   } else if (stylePattern.test(parameter)) {
+//     const srcAttribute = scriptPattern.exec(parameter)[1];
+//     const script = document.createElement('script');
+//     script.src = srcAttribute;
+//     document.head.appendChild(script);
+//   } else {
+//     const formstack = document.querySelector('formstack');
+//     if (formstack) formstack.innerHTML += parameter;
+//   }
+// };
+
+export default function decorate(block) {
+  const formstackLink = block.querySelector('a');
+  if (formstackLink && formstackLink.href) {
+    const formstackUrl = formstackLink.href;
+    block.innerHTML = '';
+    const form = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          block.append(loadScript(formstackUrl));
+          window.setTimeout(() => {
+            const saveToDom = document.createElement('script');
+            saveToDom.type = 'text/javascript';
+            const saveToDomScript = 'const formstackDiv = document.querySelector(\'formstack\'); formstackDiv.innerHTML = window.formStackContent.value;';
+            saveToDom.append(saveToDomScript);
+            document.head.appendChild(saveToDom);
+          }, 3000);
+        }
+      });
+    });
+    form.observe(block);
+  } else {
+    block.remove();
+  }
+}
