6ba12135a5e1d795faac76485115be018a204e4c
Amol Anand <amol.anand@gmail.com>
2023-05-01 15:25:47 -0400

    delay block and add chevrons

--- caesars-palace/blocks/mini-form/mini-form.js
+++ caesars-palace/blocks/mini-form/mini-form.js
@@ -43,59 +43,64 @@ export default async function decorate(block) {
   const { type, title } = cfg;
   // Clear out the block contents
   block.innerHTML = '';
-  const products = await getProducts(type);
-  // Create structure
-  const heading = createTag('h6', {}, title);
-  const dropdownMenu = createTag('ul', { class: 'dropdown-menu' });
-  let buttonText;
-  products.forEach((product) => {
-    let attrs = {};
-    if (product.secondaryUrl) {
-      attrs = {
-        'data-url': product.secondaryUrl,
-        'data-newtab': product.secondaryUrlNewTab,
-      };
-    }
-    buttonText = product.buttonText;
-    const li = createTag('li', attrs, product.productTitle);
-    dropdownMenu.append(li);
-  });
-  const dropdownLabel = createTag('div', { class: 'dropdown-label' }, `Select ${type}`);
-  const dropdownButton = createTag('a', { href: '', class: 'button primary' }, buttonText);
-  const strong = document.createElement('strong');
-  strong.append(dropdownButton);
-  const dropdownButtonContainer = createTag('p', { class: 'button-container' }, strong);
-  const dropdownContainer = createTag('div', { class: 'dropdown-container' }, [dropdownLabel, dropdownMenu]);
-  const divider = createTag('div', { class: 'divider' }, '');
-  const dropdown = createTag('div', { class: 'dropdown' }, [heading, divider, dropdownContainer, dropdownButtonContainer]);
+  // Delaying the logic
+  window.setTimeout(async () => {
+    const products = await getProducts(type);
+    if (products) {
+      // Create structure
+      const heading = createTag('h6', {}, title);
+      const dropdownMenu = createTag('ul', { class: 'dropdown-menu' });
+      let buttonText;
+      products.forEach((product) => {
+        let attrs = {};
+        if (product.secondaryUrl) {
+          attrs = {
+            'data-url': product.secondaryUrl,
+            'data-newtab': product.secondaryUrlNewTab,
+          };
+        }
+        buttonText = product.buttonText;
+        const li = createTag('li', attrs, product.productTitle);
+        dropdownMenu.append(li);
+      });
+      const dropdownLabel = createTag('div', { class: 'dropdown-label' }, `Select ${type}`);
+      const dropdownButton = createTag('a', { href: '', class: 'button primary' }, buttonText);
+      const strong = document.createElement('strong');
+      strong.append(dropdownButton);
+      const dropdownButtonContainer = createTag('p', { class: 'button-container' }, strong);
+      const dropdownContainer = createTag('div', { class: 'dropdown-container' }, [dropdownLabel, dropdownMenu]);
+      const divider = createTag('div', { class: 'divider' }, '');
+      const dropdown = createTag('div', { class: 'dropdown' }, [heading, divider, dropdownContainer, dropdownButtonContainer]);
 
-  // Event listeners to show/hide the dropdown
-  // When you click on the main selection
-  dropdownContainer.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('show');
-    dropdownContainer.classList.toggle('show');
-  });
-  // Or when you click outside the dropdown area
-  document.addEventListener('click', (event) => {
-    const isClickInside = dropdownContainer.contains(event.target);
-    if (!isClickInside) {
-      if (dropdownMenu.classList.contains('show')) dropdownMenu.classList.toggle('show');
-      if (dropdownContainer.classList.contains('show')) dropdownContainer.classList.toggle('show');
-    }
-  });
+      // Event listeners to show/hide the dropdown
+      // When you click on the main selection
+      dropdownContainer.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('show');
+        dropdownContainer.classList.toggle('show');
+      });
+      // Or when you click outside the dropdown area
+      document.addEventListener('click', (event) => {
+        const isClickInside = dropdownContainer.contains(event.target);
+        if (!isClickInside) {
+          if (dropdownMenu.classList.contains('show')) dropdownMenu.classList.toggle('show');
+          if (dropdownContainer.classList.contains('show')) dropdownContainer.classList.toggle('show');
+        }
+      });
 
-  // Event listener when an item in the dropdown is selected
-  dropdownMenu.addEventListener('click', (event) => {
-    const selectedItem = event.target;
-    // Set the label
-    dropdownLabel.textContent = selectedItem.textContent;
-    // Update the button url
-    const { url, newtab } = selectedItem.dataset;
-    if (url && newtab) {
-      dropdownButton.setAttribute('href', url);
-      if (newtab === 'true') dropdownButton.setAttribute('target', '_blank');
-    }
-  });
+      // Event listener when an item in the dropdown is selected
+      dropdownMenu.addEventListener('click', (event) => {
+        const selectedItem = event.target;
+        // Set the label
+        dropdownLabel.textContent = selectedItem.textContent;
+        // Update the button url
+        const { url, newtab } = selectedItem.dataset;
+        if (url && newtab) {
+          dropdownButton.setAttribute('href', url);
+          if (newtab === 'true') dropdownButton.setAttribute('target', '_blank');
+        }
+      });
 
-  block.append(dropdown);
+      block.append(dropdown);
+    }
+  }, 3000);
 }
